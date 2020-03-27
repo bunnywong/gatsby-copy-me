274b809c08c2d449bc047e58ec82ea503568ba62
BunnY <w@bunnywong.com>
2020-03-27 13:01:44 +0800

    update pwa info

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-copy-me`,
+        short_name: `Copy Me`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
